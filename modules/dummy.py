54a4c095e7e5381b4a54a0973270978ce1f7dca0
icoxfog417 <icoxfog417@yahoo.co.jp>
2020-12-30 00:28:57 +0900

    Add numpy style comment

--- modules/dummy.py
+++ modules/dummy.py
@@ -1,2 +1,14 @@
 def x(i: int) -> int:
+    """Dummy function
+
+    Parameters
+    ----------
+    i : int
+        input parameter
+
+    Returns
+    -------
+    int
+        return input
+    """
     return i
